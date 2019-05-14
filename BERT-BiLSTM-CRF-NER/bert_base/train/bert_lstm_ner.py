effb9855dd7330b034405108c05522e9bfa249ea
zle1992 <625015879@qq.com>
2019-05-14 22:30:30 +0800

    fix

--- BERT-BiLSTM-CRF-NER/bert_base/train/bert_lstm_ner.py
+++ BERT-BiLSTM-CRF-NER/bert_base/train/bert_lstm_ner.py
@@ -896,13 +896,16 @@ def main(_):
         
         if FLAGS.testisdev:
             predict_file = os.path.join(FLAGS.output_dir, "predict_dev.tf_record")
+            filed_based_convert_examples_to_features(predict_examples, label_list,
+                                                             FLAGS.max_seq_length, tokenizer,
+                                                         predict_file, FLAGS.output_dir, mode="test")
         else:
             predict_file = os.path.join(FLAGS.output_dir, "predict.tf_record")
 
-        #if not os.path.exists(predict_file) or FLAGS.testisdev :
-        filed_based_convert_examples_to_features(predict_examples, label_list,
-                                                     FLAGS.max_seq_length, tokenizer,
-                                                     predict_file, FLAGS.output_dir, mode="test")
+            if not os.path.exists(predict_file) :
+                filed_based_convert_examples_to_features(predict_examples, label_list,
+                                                             FLAGS.max_seq_length, tokenizer,
+                                                         predict_file, FLAGS.output_dir, mode="test")
 
         tf.logging.info("***** Running prediction*****")
         tf.logging.info("  Num examples = %d", len(predict_examples))
